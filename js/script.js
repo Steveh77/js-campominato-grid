3f5cf8173cb0378218af01bd461ae37ebbb4c36b
Steveh77 <stederosa76@gmail.com>
2022-06-30 15:36:51 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -17,3 +17,30 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
+
+
+// #MILESTONE 2
+// Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+
+const grid = document.getElementById('grid')
+
+// creazione celle
+let rows = 10
+let cells = 10
+const gridCells = rows * cells
+
+for (let i = 1; i <= gridCells; i++) {
+    const cell = document.createElement('div')
+    grid.appendChild(cell)
+    cell.className = 'cell'
+    
+}
+
+// #MILESTONE 3
+// In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
+
+
+
+
